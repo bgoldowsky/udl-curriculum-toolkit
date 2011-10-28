23078eff123c75fd601a88221d040a1071801baf
lmccormack@cast.org <lmccormack@cast.org@e8d89da5-3253-cd36-4140-101d3dbf431e>
2011-10-28 13:59:33 +0000

    Added - used in functions.js and annotated images.js

---
+++ trunk/example/theme/js/lang/en.js
@@ -0,0 +1,24 @@
+var lang = new Array();
+
+lang = {
+    'EXPAND'                : 'Show',
+    'COLLAPSE'              : 'Hide',
+
+    'ANNOTATE_LABELS'       : 'View',
+    'ANNOTATE_SHOW'         : 'image with hotspots',
+    'ANNOTATE_HIDE'         : 'image',
+    'ANNOTATE_ADD_NOTE'     : 'Add Note',
+    'ANNOTATE_OK'           : 'Ok',
+    'ANNOTATE_CANCEL'       : 'Cancel',
+    'ANNOTATE_DELETE'       : 'Delete',
+    'ANNOTATE_ERROR_SAVE'   : 'An error occured saving that note.',
+    'ANNOTATE_ERROR_DELETE' : 'An error occured deleting that note.',
+
+    'IMGVIEW_VIEW'          : 'View',
+    'IMGVIEW_IMAGE'         : 'image',
+    'IMGVIEW_HOTSPOT'       : 'image with hotspots',
+    'IMGVIEW_MYDRAWING'     : 'my version',
+
+    'DSLIDE_PREV'           : '&lt; Prev',
+    'DSLIDE_NEXT'           : 'Next &gt;',
+};
