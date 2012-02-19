700225c10013f8b750ae383d58546caa16d6f75b
lmccormack@cast.org <lmccormack@users.noreply.github.com>
2012-02-19 06:02:25 +0000

    new icon for table responses

---
+++ example/theme/js/jqgrid/jquery.jqGrid.cwm.js
@@ -0,0 +1,16 @@
+function cwmImportGrid(tableId, textAreaId) {
+
+    var table = $("#"+tableId);
+    var textArea = $("#"+textAreaId);
+
+    console.log("import the values from the textarea to the table here");
+    }
+    
+    
+function cwmExportGrid(tableId, textAreaId) {
+
+    var table = $("#"+tableId);
+    var textArea = $("#"+textAreaId);
+
+    console.log("export the values from the table to the textArea");
+    }    
