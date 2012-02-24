f7b67060d41851a7adbbed9b232cf891a9a5bd1a
lmccormack@cast.org <lmccormack@cast.org@e8d89da5-3253-cd36-4140-101d3dbf431e>
2012-02-24 23:26:57 +0000

    grid loading from url

--- trunk/example/theme/js/editablegrid/Editablegrid_cast.js
+++ trunk/example/theme/js/editablegrid/Editablegrid_cast.js
@@ -1,7 +1,11 @@
 function cwmImportGrid(divId, textAreaId, url, readonly) {
-	
+
     editableGrid = new EditableGrid("DemoGrid"); 
-	editableGrid.tableLoaded = function() { this.renderGrid(divId, "testgrid"); }; //testgrid is class of table
-	editableGrid.loadJSON(url);
+	console.log("YEAH here we are");
     
+	editableGrid.tableLoaded = function() { this.renderGrid(divId, "testgrid"); }; //testgrid is class of table
+	console.log("YEAH here we are 2");
+	editableGrid.loadJSON(url);
+	console.log("YEAH here we are 3");
+
 }
