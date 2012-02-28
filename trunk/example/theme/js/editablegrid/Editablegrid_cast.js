ce82858d382d516e0a855eba0fbad84c422d4f00
lmccormack@cast.org <lmccormack@cast.org@e8d89da5-3253-cd36-4140-101d3dbf431e>
2012-02-28 20:59:47 +0000

    current state of table js not reading url data

--- trunk/example/theme/js/editablegrid/Editablegrid_cast.js
+++ trunk/example/theme/js/editablegrid/Editablegrid_cast.js
@@ -1,10 +1,10 @@
 var myObject;
 
-function cwmImportGrid(divId, textAreaId, url, readonly) {
+function cwmImportGrid(divId, url, readonly) {
 	
     editableGrid = new EditableGrid("DemoGrid");     
 	editableGrid.tableLoaded = function() { 
-		if(readonly==true) {
+		if(readonly=="true") {
 			for ( i=0; i < editableGrid.getColumnCount(); i++) {
 				editableGrid.getColumn(i).editable = false;												
 			}			
@@ -15,14 +15,19 @@ function cwmImportGrid(divId, textAreaId, url, readonly) {
 	editableGrid.loadJSON(url);
 	this.initializeGrid();
 
-	//load string into var
-	var data = '{"metadata":[	{"name":"c1","label":"Column 1","datatype":"string","editable":true},	{"name":"c2","label":"Column 2","datatype":"string","editable":true},	{"name":"c3","label":"Column 3","datatype":"string","editable":true},	{"name":"c4","label":"Column 4","datatype":"string","editable":true},	{"name":"c5","label":"Column 5","datatype":"string","editable":true}],"data":[	{"id":1, "values":{"c1":"uk","c2":33,"c3":"Duke","c4":"Patience","c5":"1.842"}},	{"id":2, "values":{"c1":"uk","c2":33,"c3":"Duke","c4":"Patience","c5":"1.842"}},	{"id":3, "values":{"c1":"uk","c2":33,"c3":"Duke","c4":"Patience","c5":"1.842"}},	{"id":4, "values":{"c1":"uk","c2":33,"c3":"Duke","c4":"Patience","c5":"1.842"}},	{"id":5, "values":{"c1":"uk","c2":33,"c3":"Duke","c4":"Patience","c5":"1.842"}}]}';
+	var data;
+	var dataGetter = $.get(url, function() {
+		data = dataGetter.responseText;
+	  });
+
 	//parse string into object
 	myObject = JSON.parse(data);
 }
 
 function cwmExportGrid(textAreaId) {
-	jQuery('#'+textAreaId).text(JSON.stringify(myObject));  				
+	alert(JSON.stringify(myObject));
+	jQuery('#'+textAreaId).text(JSON.stringify(myObject));  
+	
 }
 
 function initializeGrid() 
