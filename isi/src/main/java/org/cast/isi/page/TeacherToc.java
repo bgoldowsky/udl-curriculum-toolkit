d1555242d28a025bac318c8fa6bcc86c7add5256
bgoldowsky@gmail.com <bgoldowsky@cast.org>
2015-05-15 13:50:33 +0000

    Switch to stable CWM release.  Update copyright year.

--- isi/src/main/java/org/cast/isi/page/TeacherToc.java
+++ isi/src/main/java/org/cast/isi/page/TeacherToc.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 CAST, Inc.
+ * Copyright 2011-2015 CAST, Inc.
  *
  * This file is part of the UDL Curriculum Toolkit:
  * see <http://code.google.com/p/udl-curriculum-toolkit>.
